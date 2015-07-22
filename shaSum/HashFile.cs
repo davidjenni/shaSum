ade31032965045e193fe373ab629131bbc56c1d7
david@DAVID-PC <david@davidjenni.com>
2015-07-22 12:47:40 -0700

    crude and functional for SHA256 hashing

---
+++ shaSum/HashFile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace shaSum
+{
+    public class HashFile
+    {
+        HashAlgorithm hashAlgorithm;
+
+        public HashFile(HashAlgorithm hashAlgorithm)
+        {
+            this.hashAlgorithm = hashAlgorithm;
+        }
+
+        public string Calculate(string fileName)
+        {
+            string fullPathFileName = Path.GetFullPath(fileName);
+            if (!File.Exists(fileName))
+            {
+                throw new ArgumentException("File '{0}' not found.", fullPathFileName);
+            }
+            using (var fileStream = new FileStream(fullPathFileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                return Calculate(fileStream);
+            }
+        }
+
+        public string Calculate(Stream stream)
+        {
+            var hashedData = this.hashAlgorithm.ComputeHash(stream);
+            return ToHexString(hashedData);
+        }
+
+        static string ToHexString(byte[] value)
+        {
+            return new System.Runtime.Remoting.Metadata.W3cXsd2001.SoapHexBinary(value).ToString();
+        }
+    }
+}
---
+++ shaSum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace shaSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Usage();
+                Environment.Exit(1);
+            }
+            var fileName = args[0];
+            var hashFile = new HashFile(GetHashingAlgorithm());
+            var hash = hashFile.Calculate(fileName);
+            Console.WriteLine(string.Format("{0}: {1}", fileName, hash));
+        }
+
+        static HashAlgorithm GetHashingAlgorithm(string hashName = "sha256")
+        {
+            switch (hashName.ToUpperInvariant())
+            {
+                case "SHA256":
+                    return new SHA256CryptoServiceProvider();
+                default:
+                    throw new NotImplementedException(string.Format("support for hash algorithm '{0}'  is not implemented yet.", hashName));
+            }
+        }
+
+        static void Usage()
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("shaSum.exe <fileName>");
+        }
+    }
+}
